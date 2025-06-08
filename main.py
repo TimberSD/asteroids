61fceddf812fedc201ddc065b30c3f075684baed
TimberSD <timdwells@gmail.com>
2025-06-08 14:11:22 -0700

    D: update to main, ch2 l2 finished

--- main.py
+++ main.py
@@ -2,19 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-pygame.init()
 from constants import *
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-while True:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
     screen.fill("black")
     pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT():
-            return
+
 
 if __name__ == "__main__":
     main()
