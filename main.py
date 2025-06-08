a65a21d5246dd188e429a4e0e0e6d80e846f6649
TimberSD <timdwells@gmail.com>
2025-06-08 13:52:06 -0700

    C: third asteroids commit

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 while True:
-    fill(000000)
-    display.flip()
+    screen.fill("black")
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            return
 
 if __name__ == "__main__":
     main()
