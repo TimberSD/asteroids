346e5baddea9f4d288f3783b71d82ae120a460e2
TimberSD <timdwells@gmail.com>
2025-06-11 15:02:50 -0700

    F: CH2 asteroids finished

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for o in drawable:
+            o.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
                 
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import *
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -14,6 +14,27 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+# Player Sprite draw    
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+# Player movement rotation    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+# Player Move method
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+# Player update method
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+# Forward and reverse keys
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
+    
